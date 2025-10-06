eae42e2380a590bc4a7c12b55f1e285267e1a839
1G4S <igor.sarnowski.21@gmail.com>
2025-10-06 15:34:22 +0200

    refactor(bronze): rename raw tables with _raw suffix

--- bronze/01_create_schema_bronze.sql
+++ bronze/01_create_schema_bronze.sql
@@ -1,14 +1,25 @@
+-- ==========================================================
+-- SCRIPT: bronze/01_create_schema_bronze.sql
+-- PURPOSE: Create the BRONZE schema and technical load log
+-- AUTHOR: Igor
+-- DATE: 2025-10-06
+-- ==========================================================
+
+-- 1. Create schema
 CREATE SCHEMA IF NOT EXISTS bronze;
 
-CREATE TABLE bronze.load_log(
+-- 2. Create load log table
+
+CREATE TABLE IF NOT EXISTS bronze.load_log(
     load_id BIGSERIAL PRIMARY KEY,
     table_name TEXT NOT NULL,
     file_name TEXT,
     rows_loaded INT,
-    load_start TIMESTAMPZ DEFAULT now(),
-    load_end TIMESTAMPZ,
+    load_start TIMESTAMPTZ DEFAULT now(),
+    load_end TIMESTAMPTZ,
     status TEXT,
     notes TEXT
 );
 
+-- 3. Set default path
 SET search_path TO bronze, public;
--- bronze/02_create_raw_tables.sql
+++ bronze/02_create_raw_tables.sql
@@ -1,25 +1,29 @@
--- ====================================================
--- STEP: Creating tables with raw data in bronze layer 
--- ====================================================
+-- ==========================================================
+-- SCRIPT: bronze/02_create_raw_tables.sql
+-- PURPOSE: Create raw tables in bronze layer
+-- AUTHOR: Igor
+-- DATE: 2025-10-06
+-- ==========================================================
+
 
 -- =======================
--- TABLE: bronze.customers
+-- TABLE: bronze.customers_raw
 -- SOURCE: data/olist_customers_dataset.csv
 -- =======================
-CREATE TABLE IF NOT EXISTS bronze.customers(
+CREATE TABLE IF NOT EXISTS bronze.customers_raw(
     customer_id TEXT,
     customer_unique_id TEXT,
     customer_zip_code_prefix INT,
     customer_city TEXT,
-    customer_state TEXT
+    customer_state TEXT,
     ingested_at TIMESTAMPTZ DEFAULT now()
 );
 
 -- =======================
--- TABLE: bronze.geolocation
+-- TABLE: bronze.geolocation_raw
 -- SOURCE: data/olist_geolocation_dataset.csv
 -- =======================
-CREATE TABLE IF NOT EXISTS bronze.geolocation(
+CREATE TABLE IF NOT EXISTS bronze.geolocation_raw(
     geolocation_zip_code_prefix INT,
     geolocation_lat FLOAT,
     geolocation_lng FLOAT,
@@ -29,25 +33,25 @@ CREATE TABLE IF NOT EXISTS bronze.geolocation(
 );
 
 -- =======================
--- TABLE: bronze.order_items
+-- TABLE: bronze.order_items_raw
 -- SOURCE: data/olist_order_items_dataset.csv
 -- =======================
-CREATE TABLE IF NOT EXISTS bronze.order_items(
+CREATE TABLE IF NOT EXISTS bronze.order_items_raw(
     order_id TEXT,
     order_item_id TEXT,
     seller_id TEXT,
-    shipping_limit_date DATETIME,
+    shipping_limit_date TIMESTAMP,
     price DECIMAL(10,2),
     freight_value DECIMAL(10,2),
     ingested_at TIMESTAMPTZ DEFAULT now()
 );
 
 -- =======================
--- TABLE: bronze.order_payments
+-- TABLE: bronze.order_payments_raw
 -- SOURCE: data/olist_order_payments_dataset.csv
 -- =======================
 
-CREATE TABLE IF NOT EXISTS bronze.order_payments(
+CREATE TABLE IF NOT EXISTS bronze.order_payments_raw(
     order_id TEXT,
     payment_sequential INT,
     payment_type TEXT,
@@ -57,41 +61,41 @@ CREATE TABLE IF NOT EXISTS bronze.order_payments(
 );
 
 -- =======================
--- TABLE: bronze.order_reviews
+-- TABLE: bronze.order_reviews_raw
 -- SOURCE: data/olist_order_reviews_dataset.csv
 -- =======================
-CREATE TABLE IF NOT EXISTS bronze.order_reviews(
+CREATE TABLE IF NOT EXISTS bronze.order_reviews_raw(
     review_id TEXT,
     order_id TEXT,
     review_score INT,
     review_comment_title TEXT,
     review_comment_message TEXT,
-    review_creation_date DATETIME,
-    review_answer_timestamp DATETIME,
+    review_creation_date TIMESTAMP,
+    review_answer_timestamp TIMESTAMP,
     ingested_at TIMESTAMPTZ DEFAULT now()
-)
+);
 
 -- =======================
--- TABLE: bronze.orders
+-- TABLE: bronze.orders_raw
 -- SOURCE: data/olist_orders_dataset.csv
 -- =======================
-CREATE TABLE IF NOT EXISTS bronze.orders(
+CREATE TABLE IF NOT EXISTS bronze.orders_raw(
     order_id TEXT,
     customer_id TEXT,
     order_status TEXT,
-    order_purchase_timestamp DATETIME,
-    order_approved_at DATETIME,
-    order_delivered_career_date DATETIME,
-    order_delivered_customer_date DATETIME,
-    order_estimated_delivery_date DATETIME,
+    order_purchase_timestamp TIMESTAMP,
+    order_approved_at TIMESTAMP,
+    order_delivered_career_date TIMESTAMP,
+    order_delivered_customer_date TIMESTAMP,
+    order_estimated_delivery_date TIMESTAMP,
     ingested_at TIMESTAMPTZ DEFAULT now()
-)
+);
 
 -- =======================
--- TABLE: bronze.products
+-- TABLE: bronze.products_raw
 -- SOURCE: data/olist_products_dataset.csv
 -- =======================
-CREATE TABLE IF NOT EXISTS bronze.products(
+CREATE TABLE IF NOT EXISTS bronze.products_raw(
     product_id TEXT,
     product_category_name TEXT,
     product_name_length INT,
@@ -102,26 +106,26 @@ CREATE TABLE IF NOT EXISTS bronze.products(
     product_height_cm INT,
     product_width_cm INT,
     ingested_at TIMESTAMPTZ DEFAULT now()
-)
+);
 
 -- =======================
--- TABLE: bronze.sellers
+-- TABLE: bronze.sellers_raw
 -- SOURCE: data/olist_sellers_dataset.csv
 -- =======================
-CREATE TABLE IF NOT EXISTS bronze.sellers(
+CREATE TABLE IF NOT EXISTS bronze.sellers_raw(
     seller_id TEXT,
     seller_zip_code_prefix TEXT,
     seller_city TEXT,
     seller_state TEXT,
     ingested_at TIMESTAMPTZ DEFAULT now()
-)
+);
 
 -- =======================
--- TABLE: bronze.product_category_name_translation
+-- TABLE: bronze.product_category_name_translation_raw
 -- SOURCE: data/product_category_name_translation.csv
 -- =======================
-CREATE TABLE IF NOT EXISTS bronze.product_category_name_translation(
+CREATE TABLE IF NOT EXISTS bronze.product_category_name_translation_raw(
     product_category_name TEXT,
     product_category_name_english TEXT,
     ingested_at TIMESTAMPTZ DEFAULT now()
-)
+);
