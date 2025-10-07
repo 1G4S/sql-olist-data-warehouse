fbe9f429a335f75d01c2cace03503f677c6db41b
1G4S <igor.sarnowski.21@gmail.com>
2025-10-07 20:03:15 +0200

    feat(bronze): load raw data to order_items, order_payments, order_reviews, orders, products, sellers, product category name translation and logs to load_log table

--- bronze/load_raw_data.sql
+++ bronze/load_raw_data.sql
@@ -10,8 +10,6 @@
 -- TABLE: bronze.customers_raw
 -- ================
 
-BEGIN;
-
 -- 1. Delete all rows from customers
 TRUNCATE TABLE bronze.customers_raw;
 
@@ -31,29 +29,25 @@ INSERT INTO bronze.load_log(
 )
 VALUES (
 	'bronze.customers_raw',
-	'olist_customers_dataset/csv',
+	'olist_customers_dataset.csv',
 	(SELECT COUNT(*) FROM bronze.customers_raw),
 	now(),
 	now(),
 	'SUCCESS',
 	NULL
-)
-
-COMMIT;
+);
 
 -- ================
 -- STEP: Load raw data to geolocation table
 -- TABLE: bronze.geolocation_raw
 -- ================
 
-BEGIN;
-
 -- 1. Delete all rows from geolocation_raw table
 TRUNCATE TABLE bronze.geolocation_raw;
 
 -- 2. Load raw data to geolocation_raw table
-COPY bronze.geolocation_raw(geolocation_zip_code_prefix,geolocation_lat,geolocation_lng,geolocation_city,geolocation_state)
-FROM '/Users/igorsarnowski/e_commerce_analysis_sql/data/olist_geolocation_dataset.csv' WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODIN 'UTF8');
+COPY bronze.geolocation_raw(geolocation_zip_code_prefix, geolocation_lat, geolocation_lng, geolocation_city, geolocation_state)
+FROM '/Users/igorsarnowski/e_commerce_analysis_sql/data/olist_geolocation_dataset.csv' WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
 
 -- 3. Insert information about load to load_log table
 INSERT INTO bronze.load_log(
@@ -75,4 +69,226 @@ VALUES(
 	NULL
 );
 
-COMMIT;
+-- ================
+-- STEP: Load raw data to order_items
+-- TABLE: bronze.order_items_raw
+-- ================
+
+-- 1. Delete rows from bronze.order_items_raw table
+TRUNCATE TABLE bronze.order_items_raw;
+
+-- 2. Load raw data to bronze.order_items_raw table
+COPY bronze.order_items_raw(order_id, order_item_id, product_id, seller_id, shipping_limit_date, price, freight_value)
+FROM '/Users/igorsarnowski/e_commerce_analysis_sql/data/olist_order_items_dataset.csv' WITH (FORMAT csv, DELIMITER ',', HEADER true, ENCODING 'UTF8');
+
+-- 3. Insert information about load into load_log table
+INSERT INTO bronze.load_log(
+	table_name,
+    file_name,
+    rows_loaded,
+    load_start,
+    load_end,
+    status,
+    notes
+)
+VALUES(
+	'bronze.order_items_raw',
+	'olist_order_items_dataset.csv',
+	(SELECT COUNT(*) FROM bronze.order_items_raw),
+	now(),
+	now(),
+	'SUCCESS',
+	NULL
+);
+
+-- ================
+-- STEP: Load raw data to order_payments
+-- TABLE: bronze.order_payments_raw
+-- ================
+
+-- 1. Delete rows from bronze.order_payments_raw table
+TRUNCATE TABLE bronze.order_payments_raw;
+
+-- 2. Load raw data to bronze.order_payments_raw table
+COPY bronze.order_payments_raw(order_id, payment_sequential, payment_type, payment_installments, payment_value)
+FROM '/Users/igorsarnowski/e_commerce_analysis_sql/data/olist_order_payments_dataset.csv' WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
+
+-- 3. Insert information about load into load_log table
+INSERT INTO bronze.load_log(
+	table_name,
+    file_name,
+    rows_loaded,
+    load_start,
+    load_end,
+    status,
+    notes
+)
+VALUES(
+	'bronze.order_payments_raw',
+	'olist_order_payments_dataset.csv',
+	(SELECT COUNT(*) FROM bronze.order_payments_raw),
+	now(),
+	now(),
+	'SUCCESS',
+	NULL
+);
+
+-- ================
+-- STEP: Load raw data to order_reviews
+-- TABLE: bronze.order_reviews_raw
+-- ================
+
+-- 1. Delete rows from bronze.order_reviews_raw table
+TRUNCATE TABLE bronze.order_reviews_raw;
+
+-- 2. Load raw data to bronze.order_reviews_raw table
+COPY bronze.order_reviews_raw(review_id, order_id, review_score, review_comment_title, review_comment_message, review_creation_date, review_answer_timestamp)
+FROM '/Users/igorsarnowski/e_commerce_analysis_sql/data/olist_order_reviews_dataset.csv' WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
+
+-- 3. Insert information about load into load_log table
+INSERT INTO bronze.load_log(
+	table_name,
+    file_name,
+    rows_loaded,
+    load_start,
+    load_end,
+    status,
+    notes
+)
+VALUES(
+	'bronze.order_reviews_raw',
+	'olist_order_reviews_dataset.csv',
+	(SELECT COUNT(*) FROM bronze.order_reviews_raw),
+	now(),
+	now(),
+	'SUCCESS',
+	NULL
+);
+
+-- ================
+-- STEP: Load raw data to orders
+-- TABLE: bronze.orders_raw
+-- ================
+
+-- 1. Delete rows from bronze.orders_raw table
+TRUNCATE TABLE bronze.orders_raw;
+
+-- 2. Load raw data to bronze.orders_raw table
+COPY bronze.orders_raw(order_id, customer_id, order_status, order_purchase_timestamp, order_approved_at, order_delivered_carrier_date, order_delivered_customer_date, order_estimated_delivery_date)
+FROM '/Users/igorsarnowski/e_commerce_analysis_sql/data/olist_orders_dataset.csv' WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
+
+-- 3. Insert information about load into load_log table
+INSERT INTO bronze.load_log(
+	table_name,
+	file_name,
+	rows_loaded,
+	load_start,
+	load_end,
+	status,
+	notes
+)
+VALUES(
+	'bronze.orders_raw',
+	'olist_orders_dataset.csv',
+	(SELECT COUNT(*) FROM bronze.orders_raw),
+	now(),
+	now(),
+	'SUCCESS',
+	NULL
+);
+
+-- ================
+-- STEP: Load raw data to products
+-- TABLE: bronze.products_raw
+-- ================
+
+-- 1. Delete rows from bronze.products_raw table
+TRUNCATE TABLE bronze.products_raw;
+
+-- 2. Load raw data to bronze.products_raw table
+COPY bronze.products_raw(product_id, product_category_name, product_name_length, product_description_length, product_photos_qty, product_weight_g, product_length_cm, product_height_cm, product_width_cm)
+FROM '/Users/igorsarnowski/e_commerce_analysis_sql/data/olist_products_dataset.csv' WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
+
+-- 3. Insert information about load into load_log table
+INSERT INTO bronze.load_log(
+	table_name,
+	file_name,
+	rows_loaded,
+	load_start,
+	load_end,
+	status,
+	notes
+)
+VALUES(
+	'bronze.products_raw',
+	'olist_products_dataset.csv',
+	(SELECT COUNT(*) FROM bronze.products_raw),
+	now(),
+	now(),
+	'SUCCESS',
+	NULL
+);
+
+-- ================
+-- STEP: Load raw data to sellers
+-- TABLE: bronze.sellers_raw
+-- ================
+
+-- 1. Delete rows from bronze.sellers_raw table
+TRUNCATE TABLE bronze.sellers_raw;
+
+-- 2. Load raw data to bronze.sellers_raw table
+COPY bronze.sellers_raw(seller_id, seller_zip_code_prefix, seller_city, seller_state)
+FROM '/Users/igorsarnowski/e_commerce_analysis_sql/data/olist_sellers_dataset.csv' WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
+
+-- 3. Insert information about load into load_log table
+INSERT INTO bronze.load_log(
+	table_name,
+	file_name,
+	rows_loaded,
+	load_start,
+	load_end,
+	status,
+	notes
+)
+VALUES(
+	'bronze.sellers_raw',
+	'olist_sellers_dataset.csv',
+	(SELECT COUNT(*) FROM bronze.sellers_raw),
+	now(),
+	now(),
+	'SUCCESS',
+	NULL
+);
+
+-- ================
+-- STEP: Load raw data to product category name translation
+-- TABLE: bronze.product_category_name_translation_raw
+-- ================
+
+-- 1. Delete rows from bronze.product_category_name_translation_raw table
+TRUNCATE TABLE bronze.product_category_name_translation_raw;
+
+-- 2. Load raw data to bronze.product_category_name_translation_raw table
+COPY bronze.product_category_name_translation_raw(product_category_name, product_category_name_english)
+FROM '/Users/igorsarnowski/e_commerce_analysis_sql/data/product_category_name_translation.csv' WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
+
+-- 3. Insert information about load into load_log table
+INSERT INTO bronze.load_log(
+	table_name,
+	file_name,
+	rows_loaded,
+	load_start,
+	load_end,
+	status,
+	notes
+)
+VALUES(
+	'bronze.product_category_name_translation_raw',
+	'product_category_name_translation.csv',
+	(SELECT COUNT(*) FROM bronze.product_category_name_translation_raw),
+	now(),
+	now(),
+	'SUCCESS',
+	NULL
+);
